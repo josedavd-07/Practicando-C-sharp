fa0a396a1e879a99bbbdbd7a38ed810efb19c8dd
Joelanaya <anayajoel848@gmail.com>
2023-11-10 00:15:35 -0500

    Saludo Basico en C#

---
+++ SaludoBasico/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+String nombre;
+int años;
+
+
+Console.Write("Escribe tu nombre: ");
+nombre = Console.ReadLine();
+
+Console.Write("Cuantos años tienes?: ");
+años = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Hola {nombre} veo que tienes {años} años, bienvenido al grupo");
