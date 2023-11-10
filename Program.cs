8134c663f38f7565794e32243cb34c1ed9e27cc3
Jose David Carranza Angarita <jodacan2005@hotmail.com>
2023-11-09 23:14:55 -0500

    Creamos una funcion para sumar dos numeros , ademas una funcion para saludar al usuario

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace programando_en_remoto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //llamada a la funcion suma
+
+            int resultado = Suma(2, 3);
+            Console.WriteLine($"El resultado de la suma es {resultado}");
+
+          // ___________________________________________________________________
+
+
+            //llamada a la funcion saludar
+            Saludar(Console.ReadLine());
+
+            // ___________________________________________________________________
+        }
+
+
+        //funcion saludar
+        static void Saludar( string name)
+        {
+            Console.WriteLine($"Hola {name}");
+        }
+
+        //funcion para sumar numeros enteros 
+        static int Suma(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+}
